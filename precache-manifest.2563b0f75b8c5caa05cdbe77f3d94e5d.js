c899c9549be7d08d5caa4c424a4d2fc6887e7d0b
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2020-04-08 22:09:32 +0900

    Updates

---
+++ precache-manifest.2563b0f75b8c5caa05cdbe77f3d94e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96135364aeb8e203224de3e777f8ac6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e23ce7f1aec13e000af3",
+    "url": "/static/css/main.14ecfa4a.chunk.css"
+  },
+  {
+    "revision": "d32cf7227d4074ae0180",
+    "url": "/static/js/2.ae1507da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ae1507da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e23ce7f1aec13e000af3",
+    "url": "/static/js/main.5728043a.chunk.js"
+  },
+  {
+    "revision": "f16b5dc62e7075eaa71a",
+    "url": "/static/js/runtime-main.42c62ec6.js"
+  }
+]);
